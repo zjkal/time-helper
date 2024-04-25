1c6195411851680a9403657c748cda4bc1f22965
zjkal <javachina@foxmail.com>
2024-04-26 01:57:39 +0800

    更新文档

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import {defineUserConfig} from "vuepress";
-import {hopeTheme} from "vuepress-theme-hope";
+import {searchProPlugin} from "vuepress-plugin-search-pro";
+import theme from "./theme.js";
 
 export default defineUserConfig({
     base: "/",
@@ -15,17 +16,31 @@ export default defineUserConfig({
             description: "",
         },
     },
-    theme: hopeTheme({
-        plugins: {
-            searchPro: true,
-            // searchPro: {
-            //   插件选项
-            // },
-        },
-    }),
+    theme,
+    plugins: [
+        searchProPlugin({
+            // 索引全部内容
+            indexContent: true,
+            // 为分类和标签添加索引
+            customFields: [
+                {
+                    getter: (page) => page.frontmatter.category as string | string[],
+                    formatter: "分类：$content",
+                },
+                {
+                    getter: (page) => page.frontmatter.tag as string | string[],
+                    formatter: "标签：$content",
+                },
+            ],
+        }),
+    ],
     markdown: {
         code: {
             lineNumbers: false
         }
     }
+
+    // Enable it with pwa
+    // shouldPrefetch: false,
 });
+

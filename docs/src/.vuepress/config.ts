06691a97b49a709ec3ff9ac5e7cfd20ca3bbe4ff
zjkal <javachina@foxmail.com>
2024-09-18 15:11:36 +0800

    更新文档

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -18,10 +18,6 @@ export default defineUserConfig({
     },
     theme,
     bundler: viteBundler({}),
-    markdown: {
-        code: {
-            lineNumbers: false
-        }
-    }
+    markdown: {}
 });
 
